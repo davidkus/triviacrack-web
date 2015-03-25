197251f1dda4d0139e132ab9578cf24b9ed1b3c9
David Kus <davidkus04@gmail.com>
2015-03-25 19:20:11 -0400

    Adding admin role

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resque_web_constraint = lambda do |request|
+    current_user = request.env['warden'].user
+    is_admin = current_user.present? && current_user.respond_to?(:admin?) && current_user.admin?
+    request.remote_ip == '127.0.0.1' || is_admin
+  end
+
   # Resque Web
   resque_web_constraint = lambda { |request| request.remote_ip == '127.0.0.1' }
   constraints resque_web_constraint do
