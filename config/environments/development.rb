9745bbf8046001e70f512a391014fb277b95c30b
David Kus <davidkus04@gmail.com>
2015-03-26 15:58:59 -0400

    Configuring action mailer and devise emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+
+  Rails.application.routes.default_url_options[:host] = 'localhost:5000'
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["EMAIL_HOST"],
+    port: 587,
+    domain: ENV["EMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,9 +60,23 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { :host => ENV['APP_HOST'] }
+
+  Rails.application.routes.default_url_options[:host] = ENV['APP_HOST']
+
+  config.action_mailer.smtp_settings = {
+    address: ENV["EMAIL_HOST"],
+    port: 587,
+    domain: ENV["EMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"]
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
