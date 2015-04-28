c7a5b424e88ce945dbc33bb1350ff38c5b5e7ddc
David Kus <davidkus04@gmail.com>
2015-04-28 17:05:59 -0400

    Styling form error messages using semantic-ui styles
    - Overriding devise_error_messages! to use the new styling
    - Adding 'error' class to all forms so that errors are styled properly
    - Adding 'segment' class to all forms
    - Some minor visual adjustments
    - Overriding field_error_proc so that it doesn't mess with the styling on fields with errors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def error_messages(resource)
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:div, msg, class: "item") }.join
+    sentence = I18n.t("errors.messages.not_saved",
+                      count: resource.errors.count,
+                      resource: resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div class="ui error message">
+      <div class="header">#{sentence}</div>
+      <div class="ui bulleted list">
+        #{messages}
+      </div>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,7 @@
+module DeviseHelper
+
+  def devise_error_messages!
+    error_messages resource
+  end
+
+end
