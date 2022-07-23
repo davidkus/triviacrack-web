af3c412fddeb06b6d9de5e8bf98f4a6993234f9c
David Kus <davidkus@users.noreply.github.com>
2022-07-23 14:46:16 -0400

    Heroku 'release' command

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec rails server -p $PORT
+release: bundle exec rails db:migrate
 worker: QUEUE=* bundle exec rake environment resque:work
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -52,4 +50,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_22_013447) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
