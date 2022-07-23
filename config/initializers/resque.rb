3c344bcec882265bfa24b8d4433f42381320142b
David Kus <davidkus@users.noreply.github.com>
2022-07-23 12:05:57 -0400

    More config changes

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-Resque.redis = Redis.new(url: ENV["REDIS_URL"])
+Resque.redis = Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" })
 Resque.after_fork = proc { ActiveRecord::Base.establish_connection }
